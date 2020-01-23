f9b100970df0e76b36be1ad87929959bf4dbbcc0
Lyceum <48872358+cab-aranw@users.noreply.github.com>
2020-01-23 12:04:55 -0500

    Assigment Completed
    Project completed as per your request.

---
+++ CoinFlipper/CoinFlipper/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoinFlipper
+{
+    public partial class CoinFlipper : Form
+    {
+        public CoinFlipper()
+        {
+            InitializeComponent();
+        }
+
+        /**
+         * Method for the Toss button of the program.
+         * Generates a random number
+         * Compares the value obtained in random and displays head or tails
+         */
+        private void TossButton_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int value = random.Next(1, 3);
+            if (value == 1)
+            {
+                MessageBox.Show("HEAD");
+                pictureBox1.Image = global::CoinFlipper.Properties.Resources.Head;
+            }
+            else
+            {
+                MessageBox.Show("TAIL");
+                pictureBox1.Image = global::CoinFlipper.Properties.Resources.Tails;
+            }
+        }
+
+        /**
+         * Method for the clear button
+         * The image equals null
+         * 
+         */
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+    }
+}
